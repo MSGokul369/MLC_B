2b81956bf32c54869de420faec2e030187bf0a89
GOKUL M SAJI <gokulmsaji07@gmail.com>
2023-01-03 10:03:07 +0530

    Adding UART Interrupt

--- MLC_TeamB/UI/user_interface.c
+++ MLC_TeamB/UI/user_interface.c
@@ -3,7 +3,6 @@
 #include "fsl_debug_console.h"
 #include "fsl_uart.h"
 
-
 void ui_homescreen(int led_refresh_rate, int start_colour[3], int end_colour[3],
 		int colour_change_rate, int current_mode_index, int resolution[3]) {
 	PRINTF("\e[1;1H\e[2J");
@@ -13,7 +12,8 @@ void ui_homescreen(int led_refresh_rate, int start_colour[3], int end_colour[3],
 	PRINTF("\t\t\t | |__) /  \\  | |    | |__     | |     | |  | |__    \r\n");
 	PRINTF("\t\t\t |  ___/ /\\ \\ | |    |  __|    | |     | |  |  __|   \r\n");
 	PRINTF("\t\t\t | |  / ____ \\| |____| |____   | |     | |  | |____  \r\n");
-	PRINTF("\t\t\t |_| /_/    \\_\\______|______|  |_|     |_|  |______| \tVersion 1.0\r\n");
+	PRINTF(
+			"\t\t\t |_| /_/    \\_\\______|______|  |_|     |_|  |______| \tVersion 1.0\r\n");
 	PRINTF("\r\n");
 	PRINTF("\t\t\t\t Multicolor LED Controller \r\n");
 	PRINTF("\t\t\t\t    ******************* \r\n");
@@ -52,12 +52,8 @@ void ui_homescreen(int led_refresh_rate, int start_colour[3], int end_colour[3],
 	PRINTF("\r\n");
 	PRINTF("\t\t\t\t Current RGB Code : x x x \r\n");
 	PRINTF("\r\n");
-	PRINTF("\t\t\t\t 1.\tConfigure RGB LED \r\n");
-	PRINTF("\t\t\t\t 2.\tConfigure Color Pattern \r\n");
-	PRINTF("\t\t\t\t s.\tStart/Stop \r\n");
-	PRINTF("\t\t\t\t p.\tPause/Resume \r\n");
-	PRINTF("\t\t\t\t  \r\n");
-	PRINTF("\t Type index and press Enter \r\n");
+	PRINTF("\r\n");
+
 }
 
 void ui_homescreen_slave() {
@@ -125,28 +121,35 @@ void ui_rgb_code_scheme(int current_rgb_scheme_index) {
 	PRINTF("\t Select color code Scheme \r\n");
 	PRINTF("\r\n");
 	/*PRINTF("\t1.\t332 RGB \r\n");
-	PRINTF("\t2.\t444 RGB \r\n");
-	PRINTF("\t3.\t888 RGB \r\n");
-	PRINTF("\t4.\tHome \r\n");
-	PRINTF("\r\n");
-	*/
+	 PRINTF("\t2.\t444 RGB \r\n");
+	 PRINTF("\t3.\t888 RGB \r\n");
+	 PRINTF("\t4.\tHome \r\n");
+	 PRINTF("\r\n");
+	 */
 
 	switch (current_rgb_scheme_index) {
 	case 1:
 		PRINTF("\tCurrent color scheme - 332 RGB\r\n");
+		PRINTF("\r\n");
+
 		break;
 	case 2:
 		PRINTF("\tCurrent color scheme - 444 RGB\r\n");
+		PRINTF("\r\n");
+
 		break;
 	case 3:
 		PRINTF("\tCurrent color scheme - 888 RGB\r\n");
+		PRINTF("\r\n");
+
 		break;
 	default:
 		PRINTF("Data invalid\r\n");
+		PRINTF("\r\n");
+
 		break;
 	}
 	PRINTF("\r\n");
-	PRINTF("\t Type index and press Enter \r\n");
 
 }
 
@@ -161,34 +164,35 @@ void ui_configure_colour_pattern(int led_refresh_rate, int start_colour[3],
 	PRINTF("\r\n");
 	PRINTF("\t Configure Color Pattern \r\n");
 	PRINTF("\r\n");
-	PRINTF("\t1.\tStart color\t\t\t:\t%d %d %d RGB \r\n", start_colour[0],
+	PRINTF("\r\n");
+	PRINTF("\r\n");
+	PRINTF("\r\n");
+	PRINTF("\t1.\t\t\t\t\t\t:\t%d %d %d RGB \r\n", start_colour[0],
 			start_colour[1], start_colour[2]);
-	PRINTF("\t2.\tEnd color\t\t\t:\t%d %d %d RGB \r\n", end_colour[0],
-			end_colour[1], end_colour[2]);
-	PRINTF("\t3.\tColor change resolution\t\t:\t%d %d %d RGB \r\n",
-			resolution[0], resolution[1], resolution[2]);
-	PRINTF("\t4.\tColor change rate\t\t:\t%d \r\n", colour_change_rate);
-	PRINTF("\t5.\tLED refresh rate\t\t:\t%d \r\n", led_refresh_rate);
+	PRINTF("\t2.\t\t\t\t\t\t:\t%d %d %d RGB \r\n", end_colour[0], end_colour[1],
+			end_colour[2]);
+	PRINTF("\t3.\t\t\t\t\t\t:\t%d %d %d RGB \r\n", resolution[0], resolution[1],
+			resolution[2]);
+	PRINTF("\t4.\t\t\t\t\t\t:\t%d \r\n", colour_change_rate);
+	PRINTF("\t5.\t\t\t\t\t\t:\t%d \r\n", led_refresh_rate);
 	switch (current_mode_index) {
 	case 1:
-		PRINTF("\t6.\tMode\t\t\t\t:\tAuto-UP \r\n");
+		PRINTF("\t\t\t\t\t\t\t:\tAuto-UP \r\n");
 		break;
 	case 2:
-		PRINTF("\t6.\tMode\t\t\t\t:\tAuto-DOWN \r\n");
+		PRINTF("\t\t\t\t\t\t\t:\tAuto-DOWN \r\n");
 		break;
 	case 3:
-		PRINTF("\t6.\tMode\t\t\t\t:\tAuto-UP/DOWN \r\n");
+		PRINTF("\t\t\t\t\t\t\t:\tAuto-UP/DOWN \r\n");
 		break;
 	case 4:
-		PRINTF("\t6.\tMode\t\t\t\t:\tManual \r\n");
+		PRINTF("\t\t\t\t\t\t\t:\tManual \r\n");
 		break;
 	default:
-		PRINTF("\t6.\tMode\t\t\t\t:\tInvalid \r\n");
+		PRINTF("\t\t\t\t\t\t\t:\tInvalid \r\n");
 		break;
 	}
-	PRINTF("\t7.\tHome \r\n");
-	PRINTF("\t Type index and press Enter \r\n");
-
+	PRINTF("\t\t\t\t\r\n");
 }
 
 void ui_modes(int current_mode_index) {
@@ -198,14 +202,6 @@ void ui_modes(int current_mode_index) {
 	PRINTF("\t\t\t\t Palette       Version 1.0 \r\n");
 	PRINTF("\t\t\t\t Multicolor LED Controller \r\n");
 	PRINTF("\t\t\t\t    ******************* \r\n");
-	PRINTF("\r\n");
-	PRINTF("\t Select Mode \r\n");
-	PRINTF("\r\n");
-	PRINTF("\t1.\tAuto UP\r\n");
-	PRINTF("\t2.\tAuto DOWN\r\n");
-	PRINTF("\t3.\tAuto UP/DOWN\r\n");
-	PRINTF("\t4.\tManual\r\n");
-	PRINTF("\t5.\tGo back\r\n");
 	PRINTF("\r\n");
 	switch (current_mode_index) {
 	case 1:
@@ -225,7 +221,7 @@ void ui_modes(int current_mode_index) {
 		break;
 	}
 	PRINTF("\r\n");
-	PRINTF("\t Type index and press Enter \r\n");
+	PRINTF("\t Select Mode \r\n");
 
 }
 
@@ -251,25 +247,29 @@ void master_ui(void) {
 				colour_change_rate, current_mode_index, resolution);
 		while (1) {
 			input_index = 0;
-			SCANF("%c", &input_index);
-			if ((input_index > 48 && input_index < 51)
-					|| (input_index == 's' || input_index == 'p')) {
+
+			char home_screen__menu[4][30] = { "Configure RGB LED",
+					"Configure Color Pattern ", "Start/Stop ", "Pause/Resume " };
+			input_index = arrow_key_navigate(home_screen__menu, 4, 26, 10);
+
+			if ((input_index > 0 && input_index < 5)) {
 				break;
 			} else {
 				PRINTF("\r\n\tInvalid Entry!\r\n\tTry again...");
 				continue;
 			}
 		}
-		PRINTF("\r\n\tSelected Index : %c", input_index);
-		PRINTF("\r\n\tPlease wait...");
-		ui_delay(5000000);
-
-		if (input_index == '1') {
+		/*PRINTF("\r\n\tSelected Index : %c", input_index);
+		 PRINTF("\r\n\tPlease wait...");
+		 ui_delay(5000000);
+		 */
+		if (input_index == 1) {
 			while (1) {
 				ui_rgb_code_scheme(curent_rgb_scheme_index);
 
-				char colour_scheme_menu[4] [30] = {"332 RGB","444 RGB", "888 RGB", "Home"};
-					input_index = arrow_key_navigate(colour_scheme_menu, 4, 9, 0);
+				char colour_scheme_menu[4][30] = { "332 RGB", "444 RGB",
+						"888 RGB", "Home" };
+				input_index = arrow_key_navigate(colour_scheme_menu, 4, 9, 10);
 
 				if (input_index == 1) {
 					while (1) {
@@ -304,21 +304,16 @@ void master_ui(void) {
 					PRINTF("\r\n\tInvalid data received");
 				}
 			}
-		} else if (input_index == '2') {
+		} else if (input_index == 2) {
 			while (1) {
 				ui_configure_colour_pattern(led_refresh_rate, start_color,
 						end_color, colour_change_rate, current_mode_index,
 						resolution);
+				char colour_scheme_menu[7][30] = { "Start color", "End color",
+						"Color change resolution", "Color change rate",
+						"LED refresh rate", "Mode", "Home", };
+				input_index = arrow_key_navigate(colour_scheme_menu, 7, 8, 10);
 
-				while (1) {
-					SCANF("%d", &input_index);
-					if (input_index > 0 && input_index < 8) {
-						break;
-					} else {
-						PRINTF("\r\n\tInvalid Entry!\r\n\tTry again...");
-						continue;
-					}
-				}
 				if (input_index == 1) {
 					while (1) {
 
@@ -484,16 +479,9 @@ void master_ui(void) {
 					//ui_delay(5000000);
 					while (1) {
 						ui_modes(current_mode_index);
-						while (1) {
-							SCANF("%d", &input_index);
-							if (input_index > 0 && input_index < 6) {
-								break;
-							} else {
-								PRINTF(
-										"\r\n\tInvalid Entry!\r\n\tTry again...");
-								continue;
-							}
-						}
+						char mode_menu[5][30] = { "Auto UP", "Auto DOWN",
+								"Auto UP/DOWN", "Manual", "Go Back" };
+						input_index = arrow_key_navigate(mode_menu, 5, 8, 10);
 						if (input_index == 1) {
 							while (1) {
 								PRINTF("\r\n\tAuto: UP mode selected.");
@@ -544,10 +532,10 @@ void master_ui(void) {
 					PRINTF("\r\n\tInvalid data received");
 				}
 			}
-		} else if (input_index == 's') {
+		} else if (input_index == 3) {
 			start_stop(led_refresh_rate, start_color, end_color,
 					colour_change_rate, current_mode_index, resolution);
-		} else if (input_index == 'p') {
+		} else if (input_index == 4) {
 			play_pause(led_refresh_rate, start_color, end_color,
 					colour_change_rate, current_mode_index, resolution);
 		} else {
@@ -669,60 +657,72 @@ void slave_ui() {
 
 }
 
-int arrow_key_navigate(char prompt[][30], int num_of_ops, int x_cor, int y_cor)
-{
-    char tx_buffer;
-    int run_flag = 1;
-    int *pointer = malloc(num_of_ops * sizeof(int));
-    int ret = 0;
-    memset(pointer,0,num_of_ops * sizeof(int));
-    pointer[0] = 1;
-    while (run_flag) {
-        PRINTF("\033[%d;%dHPress Enter to Select\r\nArrow Keys to move cursor\r\n", x_cor, y_cor);
-        for (int i = 0; i < num_of_ops; i++) {
-            if (pointer[i] == 1) {
-                            PRINTF("  ");
-                        } else {
-                            PRINTF("");
-                        }
-            PRINTF("%s", prompt[i]);
-            if (pointer[i] == 1) {
-                PRINTF("\t<==\r\n");
-            } else {
-                PRINTF("                     \r\n");
-            }
-        }
-        while (!(kUART_RxDataRegFullFlag & UART_GetStatusFlags(UART0)))
-            UART_ClearStatusFlags(UART0, kUART_RxDataRegFullFlag);
-        tx_buffer = UART_ReadByte(UART0);
-        if (tx_buffer == 66) {
-            //PRINTF("Down Arrow\r\n");
-            int temp;
-            temp = pointer[num_of_ops - 1];
-            for (int j = num_of_ops - 1; j != 0; j--) {
-                pointer[j] = pointer[j - 1];
-            }
-            pointer[0] = temp;
-        } else if (tx_buffer == 65) {
-            //PRINTF("Up Arrow\r\n");
-            int temp;
-            temp = pointer[0];
-            for (int j = 0; j != num_of_ops - 1; j++) {
-                pointer[j] = pointer[j + 1];
-            }
-            pointer[num_of_ops - 1] = temp;
-        } else if (tx_buffer == 13) {
-            //PRINTF("Enter\r\n");
-            for(int j=0; j  < num_of_ops ; j++)
-            {
-                if (pointer[j] ==  1)
-                {
-                    ret = j;
-                    run_flag = 0;
-                    break;
-                }
-            }
-        }
-    }
-    return ret + 1;
+int arrow_key_navigate(char prompt[][30], int num_of_ops, int x_cor, int y_cor) {
+	uint8_t tx_buffer = 0;
+	int run_flag = 1;
+	int *pointer = malloc(num_of_ops * sizeof(int));
+	int ret = 0;
+	memset(pointer, 0, num_of_ops * sizeof(int));
+	pointer[0] = 1;
+	UART_ClearStatusFlags(UART0, kUART_RxDataRegFullFlag);
+	while (run_flag) {
+		PRINTF("\033[%d;%dHPress Enter to Select", x_cor, y_cor);
+		PRINTF("\033[%d;%dHArrow Keys to move cursor\r\n\n", x_cor + 1, y_cor);
+		for (int i = 0; i < num_of_ops; i++) {
+			if (pointer[i] == 1) {
+				PRINTF("        ");
+			} else {
+				PRINTF("");
+			}
+			PRINTF("        %s", prompt[i]);
+			if (pointer[i] == 1) {
+				PRINTF("\t<==\r\n");
+			} else {
+				PRINTF("                     \r\n");
+			}
+		}
+		while (!(kUART_RxDataRegFullFlag & UART_GetStatusFlags(UART0)))
+			UART_ClearStatusFlags(UART0, kUART_RxDataRegFullFlag);
+
+		//PRINTF("%d", tx_buffer);
+		while (1) {
+			tx_buffer = UART_ReadByte(UART0);
+			if (tx_buffer == 66 || tx_buffer == 65 || tx_buffer == 13) {
+				break;
+			} else {
+				PRINTF("                                           ");
+				PRINTF("                                           ");
+				PRINTF("                                           ");
+				continue;
+			}
+		}
+
+		if (tx_buffer == 66) {
+			//PRINTF("Down Arrow\r\n");
+			int temp;
+			temp = pointer[num_of_ops - 1];
+			for (int j = num_of_ops - 1; j != 0; j--) {
+				pointer[j] = pointer[j - 1];
+			}
+			pointer[0] = temp;
+		} else if (tx_buffer == 65) {
+			//PRINTF("Up Arrow\r\n");
+			int temp;
+			temp = pointer[0];
+			for (int j = 0; j != num_of_ops - 1; j++) {
+				pointer[j] = pointer[j + 1];
+			}
+			pointer[num_of_ops - 1] = temp;
+		} else if (tx_buffer == 13) {
+			//PRINTF("Enter\r\n");
+			for (int j = 0; j < num_of_ops; j++) {
+				if (pointer[j] == 1) {
+					ret = j;
+					run_flag = 0;
+					break;
+				}
+			}
+		}
+	}
+	return ret + 1;
 }
